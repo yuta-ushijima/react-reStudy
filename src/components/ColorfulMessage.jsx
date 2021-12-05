83b06a5b002174cc127557d37df100927fd0086d
Yuta Ushijima <register@yuta-u.com>
2021-12-05 12:40:21 +0900

    propsのもう一つの使い方 - コンポーネントを閉じタグで利用 - propsはタグの中に書いて、子コンポーネント側でprops.childrenで受け取る

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -5,7 +5,7 @@ const ColorfulMeaages = (props) => {
     color: props.color,
     fontSize: "18px"
   };
-  return <p style={contentStyle}>{props.message}</p>;
+  return <p style={contentStyle}>{props.children}</p>;
 };
 
 export default ColorfulMeaages;
