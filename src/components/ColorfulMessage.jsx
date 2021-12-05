d962e3e769de58b2eb133c58eb30a0f03f93cbf2
Yuta Ushijima <register@yuta-u.com>
2021-12-05 12:44:07 +0900

    props応用編 - 分割代入 - オブジェクトの要素が同じプロパティ名の時、keyは不要

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const ColorfulMeaages = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: "18px"
   };
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMeaages;
